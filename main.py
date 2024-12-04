41a691a8a5d257080b0884f73d45c5865f4e783b
Mariah Warren <msmith12346@gmail.com>
2024-12-04 23:00:28 +0000

    Submitting Random Fact Generator lab

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-import checkers
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+class RandomFact(BaseModel):
+    id: int
+    fact: str
+    
+app=FastAPI()
 
-def game():
-    response = int(input("What size checkers board do you want?(Pick a size between 4 and 16):"))
-    print(checkers.build_board(response, response))
-    print(f"Empty Cells: {checkers.get_count(response, "Empty")}")
-    print(f"Red Cells: {checkers.get_count(response, "Red")}")
-    print(f"Black Cells: {checkers.get_count(response, "Black")}")
+RandomFactGenerator=[RandomFact(id=1,fact='A group of flamingos is called a flamboyance'),RandomFact(id=2,fact='There are more stars in the universe than grains of sand on Earth')]
 
+# GET
+@app.get('/fact/')
+async def get_facts():
+    return RandomFactGenerator
+@app.get('/fact/{id}')
+async def get_id(id):
+    return (i for i in RandomFactGenerator if i.id == int(id))
 
-if __name__ == "__main__":
-    game()
+# POST
+@app.post('/fact/')
+async def add_new_fact(NewRandomFact: RandomFact):
+    RandomFactGenerator.append(NewRandomFact)
+    return NewRandomFact
+
+#PUT
+@app.put('/fact/{id}')
+async def update_facts(updatedRandomFact: RandomFact, id):
+    for RandomFact in RandomFactGenerator:
+        if RandomFact.id == int(id):
+            RandomFact=updatedRandomFact
+            return RandomFact
+        return None
+
+#DELETE
+@app.delete('/fact/{id}')
+async def delete_facts(id):
+     for f in RandomFactGenerator:
+       if f.id == int(id):
+           RandomFactGenerator.remove(f)
+     return None
+    
